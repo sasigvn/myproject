3a6ef0a53ea69e34861a78600eb4a8b1e69a4a4f
Venkata Naga, Sasidhar G <sasidhar.g.venkata.naga@one.verizon.com>
2016-01-05 15:45:16 +0530

    added few

--- TestingNew.cs
+++ TestingNew.cs
@@ -1,3 +1,91 @@
 Hello,
 This is further test to do.
+adsklaskl
+sda
+sdasdal;dsal
+sdaasdsad
+sadsaddsa
+ssdasda
+sadsdasad
+dsadsadsaaaaaa
+dsadsaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+sdap[wqe
+sdaaaaaaaaaaaaaaaaa
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
+enovo unveiled TheatreMax, an immersive large screen experience on smartphones powered by Lenovo Virtual reality technology. TheatreMax can convert any multimedia content to become immersive, giving users a virtual large screen cinematic experience when viewed through a VR headset.
+
+It featuring a vibrant 5.5? Full HD display with 178-degree wide viewing angle and 1080 pixel clarity, together with color optimization and high contrast technologies to further enhance the quality of the visuals. The smartphone also comes with an advanced sound input and recording capabilities, if you are aspiring to produce your own digital content. Lenovo configured 3-mic system supported by Wolfson audio codec which effectively filter out ambient noise and greatly enhance sound quality recording. K4 note features the twin front ported stereo speakers enabled by Dolby Atoms. These minimize noise and voice distortion when playing voice clips and songs, automatically fine tuning any audio experience for listeners. Combined with a 13MP DAF rear camera and a 5MP fixed-focused selfie camera.
+
+The K4 Note has an embedded battery of 3300mAh, 64-bit MediaTek octa-core processor, Android 5.1, Dual 4G enabled Micro Sim card slots, Mali T720-MP3 GPU and 3GB of RAM combine to deliver exceptional performance experience while gaming, watching video and surfing.
+
+Storage can be expanded to 144GB (16GB internal) along with USB-OTG support while staying connected with suer-fast LTE seeds and securing features including fingerprint and NFC sensors.
+
+The Lenovo k4 NOte’s registrations for its first flash sale on January 19, 2016, will be starting today at 3 PM. Pricing for the model starts at Rs 11,999/- but with VR(virtual reality) bundle it will cost you 12499/-
+
+ 
 
